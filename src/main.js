af844427f1d7d16a1b0a7b72c539721be3ba5824
codeking <1094234526@qq.com>
2022-09-30 11:26:40 +0800

    2022 9.30 初始化项目 配置环境 安装一些包
    Signed-off-by: codeking <1094234526@qq.com>

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+/**
+ * axios
+ * pinia
+ * vue-router@4
+ * wangeditor
+ * sass
+ * naive-ui
+ */
+
+createApp(App).mount('#app')
